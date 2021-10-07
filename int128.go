857a740fbd700d040d1ef03121567ee4e52a4e90
Ichinose Shogo <shogo82148@gmail.com>
2021-10-07 23:17:14 +0900

    implement Int128.Cmp

--- int128.go
+++ int128.go
@@ -18,3 +18,19 @@ func (a Int128) Sub(b Int128) Int128 {
 	h, _ := bits.Sub64(uint64(a.H), uint64(b.H), borrow)
 	return Int128{int64(h), l}
 }
+
+func (a Int128) Cmp(b Int128) int {
+	if a.H == b.H {
+		if a.L == b.L {
+			return 0
+		} else if a.L > b.L {
+			return 1
+		} else {
+			return -1
+		}
+	} else if a.H > b.H {
+		return 1
+	} else {
+		return -1
+	}
+}
--- int128_test.go
+++ int128_test.go
@@ -88,3 +88,53 @@ func BenchmarkInt128_Sub(b *testing.B) {
 		runtime.KeepAlive(x.Sub(y))
 	}
 }
+
+func TestInt128_Cmp(t *testing.T) {
+	testCases := []struct {
+		a, b Int128
+		want int
+	}{
+		{
+			Int128{0, 0},
+			Int128{0, 0},
+			0,
+		},
+		{
+			Int128{0, 1},
+			Int128{0, 0},
+			1,
+		},
+		{
+			Int128{0, 0},
+			Int128{0, 1},
+			-1,
+		},
+		{
+			Int128{0, 0},
+			Int128{-1, 0xffff_ffff_ffff_ffff},
+			1,
+		},
+		{
+			Int128{-1, 0xffff_ffff_ffff_ffff},
+			Int128{0, 0},
+			-1,
+		},
+		{
+			Int128{-1, 0xffff_ffff_ffff_ffff},
+			Int128{-1, 0xffff_ffff_ffff_fffe},
+			1,
+		},
+		{
+			Int128{-1, 0xffff_ffff_ffff_fffe},
+			Int128{-1, 0xffff_ffff_ffff_ffff},
+			-1,
+		},
+	}
+
+	for i, tc := range testCases {
+		got := tc.a.Cmp(tc.b)
+		if got != tc.want {
+			t.Errorf("%d: %#v * %#v should %d, but %d", i, tc.a, tc.b, tc.want, got)
+		}
+	}
+}
