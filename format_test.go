25eef92e9f634541b297ca23eaab73529c4449aa
ICHINOSE Shogo <shogo82148@gmail.com>
2023-10-15 11:35:12 +0900

    implement Int128 formatter

--- format_test.go
+++ format_test.go
@@ -5,6 +5,53 @@ import (
 	"testing"
 )
 
+func TestInt128Format(t *testing.T) {
+	tests := []struct {
+		format string
+		value  Int128
+		want   string
+	}{
+		{"%d", Int128{0, 0}, "0"},
+		{"%d", Int128{0, 1}.Neg(), "-1"},
+		{"%+d", Int128{0, 0}, "+0"},
+		{"%+d", Int128{0, 1}.Neg(), "-1"},
+		{"% d", Int128{0, 0}, " 0"},
+		{"% d", Int128{0, 1}.Neg(), "-1"},
+		{"%8d", Int128{0, 0}, "       0"},
+		{"%08d", Int128{0, 0}, "00000000"},
+		{"%-8d", Int128{0, 0}, "0       "},
+		{"%+08d", Int128{0, 0}, "+0000000"},
+		{"%+8d", Int128{0, 0}, "      +0"},
+		{"% 08d", Int128{0, 0}, " 0000000"},
+
+		{"%b", Int128{0, 0xAA}, "10101010"},
+		{"%#b", Int128{0, 0xAA}, "0b10101010"},
+
+		{"%o", Int128{0, 0}, "0"},
+		{"%o", Int128{0, 1}, "1"},
+		{"%#o", Int128{0, 0}, "0"},
+		{"%#o", Int128{0, 1}, "01"},
+
+		{"%O", Int128{0, 0}, "0o0"},
+		{"%O", Int128{0, 1}, "0o1"},
+		{"%#O", Int128{0, 0}, "0o0"},
+		{"%#O", Int128{0, 1}, "0o01"},
+
+		{"%x", Int128{0, 0xabcd}, "abcd"},
+		{"%#x", Int128{0, 0xabcd}, "0xabcd"},
+
+		{"%X", Int128{0, 0xabcd}, "ABCD"},
+		{"%#X", Int128{0, 0xabcd}, "0XABCD"},
+	}
+
+	for _, tt := range tests {
+		got := fmt.Sprintf(tt.format, tt.value)
+		if got != tt.want {
+			t.Errorf("%#v: want %q, got %q", tt, tt.want, got)
+		}
+	}
+}
+
 func TestUint128Format(t *testing.T) {
 	tests := []struct {
 		format string
